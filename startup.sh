34daaada9fcc1329da1047edc6f71c3a9ed70c76
MD Misbah ur rahman <rahmanmisbahur2005@gmail.com>
2025-09-12 20:09:12 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 # Start the Streamlit frontend server
-streamlit run ui.py --server.port 8501 --server.address 0.0.0.0
+streamlit run ui.py --server.port 7860 --server.address 0.0.0.0
